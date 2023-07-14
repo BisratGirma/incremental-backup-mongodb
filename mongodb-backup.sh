c827f2acc3a1b5f0eef04668190e8215c5bab575
Bisrat Girma <bisrat@eaglelionsystems.com>
2023-07-14 12:27:32 +0300

    test done for incremental db

--- mongodb-backup.sh
+++ mongodb-backup.sh
@@ -5,17 +5,40 @@ HOST="--host"
 PORT="--port"
 DB="--database_name"
 
-# Backup directory
-BACKUP_DIR="absolute_path_of_the_backup_folder"
+# HOST="localhost"
+# PORT="27017"
+# DB="dbname"
+
+# the backup directory
+BACKUP_DIR="absolute_path_of_backup_folder"
+
+collections=$(mongosh --quiet --host "$HOST" --port "$PORT" --eval "db.getSiblingDB('mineral').getCollectionNames()" | grep -oE "[a-zA-Z0-9_]+" | awk 'BEGIN{ORS=", "} {print "\"" $0 "\""}')
+
+if [ -f "${BACKUP_DIR}last_backup_timestamp.txt" ]; then
+
+    LAST_BACKUP_TIMESTAMP=$(cat "${BACKUP_DIR}last_backup_timestamp.txt")
+else
+
+    LAST_BACKUP_TIMESTAMP=$(date -d "Sep 01 2015 00:00:00" +%FT%T%z)
+fi
+
+CURRENT_TIMESTAMP=$(date +%FT%T%z)
 
 TEMP_DIR=$(mktemp -d)
 
-# dump the Mongo database to the temporary directory
-mongodump --host "$HOST" --port "$PORT" --db "$DB" --out "$TEMP_DIR"
+# interate over collections dump mongodump to temprary file
+for collection in $collections
+do
 
-# Rename the backup file with the current timestamp
-CURRENT_TIMESTAMP=$(date +%x%X)
-mv "$TEMP_DIR/$DB" "$BACKUP_DIR$CURRENT_TIMESTAMP"
+    collection=$(echo "$collection" | tr -d '",')
+
+    # mongodump command for a collection with query and output to a temporary folder
+    mongodump --host "$HOST" --port "$PORT" --db "$DB" --collection "$collection" --query "{ \"last_modified\": { \"\$gt\": { \"\$date\": \"$LAST_BACKUP_TIMESTAMP\" } } }" --out "$TEMP_DIR"
+
+done
+
+# move mongodump file to a backup folder
+mkdir -p "$BACKUP_DIR$CURRENT_TIMESTAMP" && mv "$TEMP_DIR/$DB" "$BACKUP_DIR$CURRENT_TIMESTAMP"
 
 # Update the last backup timestamp
 echo "$CURRENT_TIMESTAMP" > "${BACKUP_DIR}last_backup_timestamp.txt"
