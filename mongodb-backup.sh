7e6ccb97a18f69719916ee61fe722de9344c24ab
Bisrat Girma <bisrat@eaglelionsystems.com>
2023-07-14 15:11:17 +0300

    comments

--- mongodb-backup.sh
+++ mongodb-backup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # MongoDB connection details
-HOST="--host"
-PORT="--port"
-DB="--database_name"
+HOST="replace with your host"
+PORT="replace with db port"
+DB="replace with db name"
 
 intial_backup_date="Sep 01 2015 00:00:00"
 
 CURRENT_TIMESTAMP=$(date +%FT%T%z)
 
 # the backup directory
-BACKUP_DIR="absolute_path_of_backup_folder"
+BACKUP_DIR="replace with absolute path of backup folder"
 
-collections=$(mongosh --quiet --host "$HOST" --port "$PORT" --eval "db.getSiblingDB('mineral').getCollectionNames()" | grep -oE "[a-zA-Z0-9_]+" | awk 'BEGIN{ORS=", "} {print "\"" $0 "\""}')
+# get list of collections of the database
+collections=$(mongosh --quiet --host "$HOST" --port "$PORT" --eval "db.getSiblingDB('mineral').getCollectionNames()" | grep -oE "[a-zA-Z0-9_]+" | awk 'BEGIN{ORS=" "} {print $0}')
 
 if [ -f "${BACKUP_DIR}last_backup_timestamp.txt" ]; then
 
@@ -22,13 +23,12 @@ else
     LAST_BACKUP_TIMESTAMP=$(date -d "$intial_backup_date" +%FT%T%z)
 fi
 
+# create temporary folder
 TEMP_DIR=$(mktemp -d)
 
 # interate over collections and dump mongodump to temprary file
 for collection in $collections
 do
-
-    collection=$(echo "$collection" | tr -d '",')
 
     # mongodump command for a collection with query and output to a temporary folder
     mongodump --host "$HOST" --port "$PORT" --db "$DB" --collection "$collection" --query "{ \"last_modified\": { \"\$gt\": { \"\$date\": \"$LAST_BACKUP_TIMESTAMP\" } } }" --out "$TEMP_DIR"
